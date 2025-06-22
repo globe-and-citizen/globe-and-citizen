2de0ee0b0ca1fa54b39f91097e7b5519bfb49a61
Marc <marclupanc@gmail.com>
2025-06-22 16:21:02 +0300

    deployment

--- src/components/AdminPanel/DropdownAction.vue
+++ src/components/AdminPanel/DropdownAction.vue
@@ -47,7 +47,7 @@ function handleView() {
   // Navigate to user profile page
   window.open(`/profile/${props.user.id}`, "_blank");
 }
-
+console.log("DropdownAction component loaded");
 function handleEdit() {
   props.onEdit(props.user);
 }
