adb526fb0c7da8e08132f2db9b53fe9f565aeba8
Marc <marclupanc@gmail.com>
2025-07-02 14:31:00 +0300

    esling html config

--- eslint.config.js
+++ eslint.config.js
@@ -25,6 +25,7 @@ export default typescriptEslint.config(
       "vue/multi-word-component-names": ["off"],
       "@typescript-eslint/no-unused-vars": ["warn"],
       "vue/require-default-prop": ["off"],
+      "vue/no-v-html": ["off"],
     },
   },
   eslintConfigPrettier
