2452edb28e0b5499dba1a48efab702492551a1f6
Marc <marclupanc@gmail.com>
2025-08-31 12:36:48 +0300

    Ts config setup

--- global.d.ts
+++ global.d.ts
@@ -1,5 +1,9 @@
+import "vue";
+
 declare module "@vue/runtime-dom" {
   interface HTMLAttributes {
     "data-slot"?: string;
+
+    [key: `data-${string}`]: string | number | boolean | undefined;
   }
 }
